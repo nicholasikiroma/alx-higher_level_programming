f8611b64f9e9fd410668b5c51c8ba8264b932d72
Nicholas Ikiroma <nicholasikiroma@gmail.com>
2022-09-20 21:13:05 -0700

    removed space

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@ def uppercase(str):
     for c in str:
         if ord(c) in range(ord('a'), ord('z') + 1):
             c = chr(ord(c) - (ord('a') - ord('A')))
-        print("{:s}".format(c), end = '')
+        print("{:s}".format(c), end ='')
     print("")
