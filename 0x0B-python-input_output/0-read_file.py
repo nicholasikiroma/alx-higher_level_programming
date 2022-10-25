01492da9921e65abd077c13446cf214d95383f4f
Nicholas Ikiroma <nicholasikiroma@gmail.com>
2022-10-25 07:43:23 -0700

    fixed print and added documentation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,6 @@
 
 
 def read_file(filename=""):
+    """function that reads a text file (UTF8) and prints it to stdout"""
     with open('filename', encoding='utf-8') as a_file:
-        return a_file.read()
+        print(a_file.read, end="")
