75f5324e5cce7bdcd0194b28dc14443eb8687637
Nicholas Ikiroma <105177945+nicholasikiroma@users.noreply.github.com>
2022-10-26 08:46:30 +0100

    Fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
-"""Module that adds all arguments to a Python list,
-    and then save them to a file
 """
+Script that adds all arguments to a Python list,
+and then save them to a file
+"""
+
 
 import sys
 
