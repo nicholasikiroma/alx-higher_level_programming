849a9e3ba9e19f4b43b598cc544f5e5f37b036b2
Nicholas Ikiroma <nicholasikiroma@gmail.com>
2022-12-01 16:39:23 -0800

    more sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,12 +1,14 @@
 -- creates the database hbtn_0d_usa and the table cities
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 -- switch database
-USE hbtn_0d_usa
+USE hbtn_0d_usa;
 
 -- Create table in database
 CREATE TABLE IF NOT EXISTS cities(
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	state_id INT NOT NULL FOREIGN KEY REFERENCES states(id),
-	name VARCHAR(256)
+	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	state_id INT NOT NULL,
+	name VARCHAR(256),
+	UNIQUE(id),
+	FOREIGN KEY (state_id) REFERENCES states(id)
 );
